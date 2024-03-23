b18934c7043ed7355a261b53c76d369dbf95654e
Pedro Camacho <pedroalmeidacamacho@gmail.com>
2024-03-22 22:20:35 -0300

    BUG NA LINHA 109 ATENÇAO

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ const filmes = [
     },
     {
      id:4,
-     nome: 'A Lagoa Azul',
+     nome: 'Batman',
      genero: 'romance',
      lancamento: 1983
 }]
@@ -106,7 +106,7 @@ btn1.addEventListener('click',()=>{
     inputUsuario.value = ''
 })
 /*
-Função corrigaaaaaa
+    Pull requestado
 */
 
 const favoritoClicado = (eventoDeClique) => {
